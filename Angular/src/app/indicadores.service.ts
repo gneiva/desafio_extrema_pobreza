634f659eea2000738f6cd8a2239df9ce911149f6
Gabriel Machado <gabriel.machado@fornecedores.sicoob.com.br>
2021-01-31 17:47:26 -0300

    Sevice para consulta ao Quarkus

---
+++ Angular/src/app/indicadores.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CotacaoService  {
+
+  private BASE_PATH = `https://localhost:8080/banco-mundial`;
+    httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+
+  constructor(private http: HttpClient, private router: Router) {
+    
+  }
+
+  public loadPobreza(idPais: String): Observable<any> {
+    return this.http.get(
+      `${this.BASE_PATH}/pais/${idPais}`,
+      this.httpOptions
+    ).pipe(
+      map((response: any) => {
+        if (response && response.resultado) {
+          return response.resultado;
+        }
+      }),
+      catchError(error =>  error)
+    );
+  }
+
+
+}
